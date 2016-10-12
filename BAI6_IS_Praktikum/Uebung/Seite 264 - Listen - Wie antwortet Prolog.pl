e9d069896f16444ee02df972f48afd8138155f77
wwwrider <amin_nabouli@web.de>
2016-10-12 15:04:06 +0200

    Letzte Übungen des Logik-Kapitels hinzugefügt

---
+++ BAI6_IS_Praktikum/Uebung/Seite 264 - Listen - Wie antwortet Prolog.pl
@@ -0,0 +1,9 @@
+%% Anfragen: Wie antwortet Prolog
+%% [a,b,c] = [a|X].
+%% [a,b,c] = [a,X].
+%% [a,b,c] = [a,b,X].
+%% [a,b,c] = [_,_|X].
+%% [a,b,c] = [_|[X]].
+%% [] = _.
+%% [] = [_].
+%% [a,b,c] = [X].
