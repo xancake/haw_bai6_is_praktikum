e2f93525b13d06dfed3e432485aba57b0b4b8185
Xancake <xanylein@web.de>
2016-12-11 22:49:32 +0100

    Praktikum 4:
    - Methoden zur Ermittlung von Nachbarschaften hinzugefügt
    - Methoden zur Ermittlung von Constraints die bestimmte Variablen
    betreffen hinzugefügt

--- BAI6_IS_Praktikum/Praktikum/Praktikum 4/src/org/haw/is/praktikum4/constraints/ConstraintNet.java
+++ BAI6_IS_Praktikum/Praktikum/Praktikum 4/src/org/haw/is/praktikum4/constraints/ConstraintNet.java
@@ -22,10 +22,6 @@ public class ConstraintNet {
 		_variablen.add(v);
 	}
 	
-	public void addAllVariables(Collection<? extends Variable> vs) {
-		_variablen.addAll(vs);
-	}
-	
 	public void addConstraint(UnaryConstraint c) {
 		_unaryConstraints.add(c);
 	}
@@ -36,6 +32,10 @@ public class ConstraintNet {
 	
 	public void addConstraint(AllUniqueConstraint c) {
 		_binaryConstraints.addAll(c.generateBinaryConstraints());
+	}
+	
+	public void addAllVariables(Collection<? extends Variable> vs) {
+		_variablen.addAll(vs);
 	}
 	
 	public void addAllUnaryConstraints(Collection<? extends UnaryConstraint> cs) {
@@ -56,5 +56,66 @@ public class ConstraintNet {
 	
 	public List<BinaryConstraint> getBinaryConstraints() {
 		return _binaryConstraints;
+	}
+	
+	/**
+	 * Gibt eine Liste aller {@link UnaryConstraint unären Constraints} für die übergebene Variable zurück.
+	 * @param v Die Variable zu der alle unären Constraints geliefert werden sollen
+	 * @return Eine Liste aller unären Constraints für {@code v}
+	 */
+	public List<UnaryConstraint> getUnaryConstraintsFor(Variable v) {
+		List<UnaryConstraint> constraints = new ArrayList<>();
+		for(UnaryConstraint c : getUnaryConstraints()) {
+			if(c.usesVariable(v)) {
+				constraints.add(c);
+			}
+		}
+		return constraints;
+	}
+	
+	/**
+	 * Gibt eine Liste aller {@link BinaryConstraint binären Constraints}, die die übergebenen Variablen benutzen zurück.
+	 * @param vi Die eine Variable
+	 * @param vj Die andere Variable
+	 * @return Eine Liste aller binären Constraints für {@code vi} und {@code vj}
+	 */
+	public List<BinaryConstraint> getBinaryConstraintsFor(Variable vi, Variable vj) {
+		List<BinaryConstraint> constraints = new ArrayList<>();
+		for(BinaryConstraint c : getBinaryConstraints()) {
+			if(c.usesVariable(vi) && c.usesVariable(vj)) {
+				constraints.add(c);
+			}
+		}
+		return constraints;
+	}
+	
+	/**
+	 * Gibt alle Nachbarn der übergebenen Variable zurück.
+	 * @param v Die Variable
+	 * @return Alle Nachbarn der Variable
+	 */
+	public List<Variable> getNeighbours(Variable v) {
+		return getNeighbours(v, 0, _variablen.size());
+	}
+	
+	/**
+	 * Gibt alle Nachbarn der übergebenen Variable zurück, die sich im Indexbereich {@code from - to} befinden.
+	 * @param v Die Variable
+	 * @param from Der Anfang des Indexbereichs (inklusive)
+	 * @param to Das Ende des Indexbereichs (exklusive)
+	 * @return Die Nachbarn der Variable im Indexbereich
+	 */
+	public List<Variable> getNeighbours(Variable v, int from, int to) {
+		List<Variable> neighbours = new ArrayList<>();
+		for(int i=from; i<to; i++) {
+			Variable vi = _variablen.get(i);
+			for(BinaryConstraint c : getBinaryConstraints()) {
+				if(c.usesVariable(v) && c.usesVariable(vi)) {
+					neighbours.add(vi);
+					break;
+				}
+			}
+		}
+		return neighbours;
 	}
 }
